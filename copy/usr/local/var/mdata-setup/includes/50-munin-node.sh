25952c11d90cf48b09dfabe4863d07cc91e3ec7a
jfqd <jfqd@blun.org>
2018-10-01 19:38:52 +0200

    there is no tpl file

--- copy/usr/local/var/mdata-setup/includes/50-munin-node.sh
+++ copy/usr/local/var/mdata-setup/includes/50-munin-node.sh
@@ -3,7 +3,6 @@
 
 MUNIN_CONF='/etc/munin/munin-node.conf'
 
-cp ${MUNIN_CONF}.tpl ${MUNIN_CONF}
 if mdata-get munin_master_allow 1>/dev/null 2>&1; then
 	echo "# mdata-get munin_master_allow" >> ${MUNIN_CONF}
 	for allow in $(mdata-get munin_master_allow); do
