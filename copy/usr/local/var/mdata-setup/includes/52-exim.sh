a0d9b88ab75f2b062744594208853ef8eac94bd7
jfqd <jfqd@blun.org>
2018-10-01 19:15:40 +0200

    add bootstrap service

---
+++ copy/usr/local/var/mdata-setup/includes/52-exim.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/bash
+if mdata-get mail_smarthost 1>/dev/null 2>&1; then
+	if mdata-get mail_adminaddr 1>/dev/null 2>&1; then
+		echo "root: $(mdata-get mail_adminaddr)" >> /etc/aliases
+	fi
+	AUTH=""
+	if mdata-get mail_auth_user 1>/dev/null 2>&1 && 
+	   mdata-get mail_auth_pass 1>/dev/null 2>&1; then
+		AUTH="--user=$(mdata-get mail_auth_user) --pass=$(mdata-get mail_auth_pass)"
+	fi
+	echo "$(mdata-get mail_smarthost) smtp --ssl $AUTH" > /etc/exim4/passwd.client
+	chmod 0640 /etc/exim4/passwd.client
+  
+  sed -i "s:dc_eximconfig_configtype='local':dc_eximconfig_configtype='satellite'" \
+    /etc/exim4/update-exim4.conf.conf
+
+  sed -i "s:dc_smarthost='':dc_smarthost='$(mdata-get mail_smarthost)'" \
+    /etc/exim4/update-exim4.conf.conf
+fi
