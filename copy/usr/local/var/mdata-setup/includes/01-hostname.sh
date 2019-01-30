b61446070fb240ede6cadd48f73d3de471cb0cb6
jfqd <jfqd@blun.org>
2019-01-30 20:25:18 +0100

    set hostname

---
+++ copy/usr/local/var/mdata-setup/includes/01-hostname.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+if mdata-get sdc:hostname 1>/dev/null 2>&1; then
+  HOSTNAME=$(mdata-get sdc:hostname)
+  hostname $HOSTNAME
+  hostname > /etc/hostname
+  tail -n +2 /etc/hosts > /etc/hosts.bak
+  echo "127.0.0.1       ${HOSTNAME}" >> /etc/hosts.bak
+  rm /etc/hosts
+  mv /etc/hosts.bak /etc/hosts
+  # echo "search $(hostname -d)">> /etc/resolv.conf
+fi
