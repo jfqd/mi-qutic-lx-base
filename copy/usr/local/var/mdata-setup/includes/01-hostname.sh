14561c4e5aaf6f6e62e2a6caac406f3a16545f62
jfqd <jfqd@blun.org>
2019-01-31 14:06:55 +0100

    fix hostname

--- copy/usr/local/var/mdata-setup/includes/01-hostname.sh
+++ copy/usr/local/var/mdata-setup/includes/01-hostname.sh
@@ -4,9 +4,6 @@ if /native/usr/sbin/mdata-get sdc:hostname 1>/dev/null 2>&1; then
   HOSTNAME=$(/native/usr/sbin/mdata-get sdc:hostname)
   hostname $HOSTNAME
   hostname > /etc/hostname
-  tail -n +2 /etc/hosts > /etc/hosts.bak
-  echo "127.0.0.1       ${HOSTNAME}" >> /etc/hosts.bak
-  rm /etc/hosts
-  mv /etc/hosts.bak /etc/hosts
-  # echo "search $(hostname -d)">> /etc/resolv.conf
+  echo "127.0.0.1       localhost ${HOSTNAME}" > /etc/hosts
+  echo "::1             localhost ip6-localhost ip6-loopback" >> /etc/hosts
 fi
