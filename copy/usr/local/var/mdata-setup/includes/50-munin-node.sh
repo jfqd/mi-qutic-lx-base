db03b6e360fd2afd328992bed4dc21903ea47083
jfqd <jfqd@blun.org>
2019-11-13 14:08:43 +0100

    add systemd health-check

--- copy/usr/local/var/mdata-setup/includes/50-munin-node.sh
+++ copy/usr/local/var/mdata-setup/includes/50-munin-node.sh
@@ -4,9 +4,9 @@
 MUNIN_CONF='/etc/munin/munin-node.conf'
 
 if /native/usr/sbin/mdata-get munin_master_allow 1>/dev/null 2>&1; then
-	echo "# mdata-get munin_master_allow" >> ${MUNIN_CONF}
-	for allow in $(/native/usr/sbin/mdata-get munin_master_allow); do
-		echo "allow ^${allow//\./\\.}$" >> ${MUNIN_CONF}
-	done
+  echo "# mdata-get munin_master_allow" >> ${MUNIN_CONF}
+  for allow in $(/native/usr/sbin/mdata-get munin_master_allow); do
+    echo "allow ^${allow//\./\\.}$" >> ${MUNIN_CONF}
+  done
   service munin-node restart
 fi
