8277615084a89785571f12fdebac6ee78daf9c32
jfqd <jfqd@blun.org>
2021-12-19 16:10:27 +0100

    add filebeats

---
+++ copy/usr/local/var/mdata-setup/includes/51-beats.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+HOSTNAME=$(/usr/bin/hostname)
+sed -i \
+    "s/host.example.com/${HOSTNAME}/" \
+    /etc/filebeat/filebeat.yml
+
+if mdata-get logstash_redis 1>/dev/null 2>&1; then
+  LOGSTASH_REDIS=$(mdata-get logstash_redis | sed "s/,/\",\"/")
+
+  sed -i \
+      "s#127.0.0.1#${LOGSTASH_REDIS}#" \
+      /etc/filebeat/filebeat.yml
+fi
+
+systemctl start filebeat
+systemctl enable filebeat
