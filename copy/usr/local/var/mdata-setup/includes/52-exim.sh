df904adbf773795fa325948a64be4855dc4b8b6a
jfqd <jfqd@blun.org>
2022-05-20 18:17:03 +0200

    fix exim config

--- copy/usr/local/var/mdata-setup/includes/52-exim.sh
+++ copy/usr/local/var/mdata-setup/includes/52-exim.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/bash
+IP=$(ifconfig eth0 | grep "inet\b" | awk '{print $2}' | cut -d/ -f1)
+HOST=$(/native/usr/sbin/mdata-get mail_smarthost)
+
 if /native/usr/sbin/mdata-get mail_smarthost 1>/dev/null 2>&1; then
   if /native/usr/sbin/mdata-get mail_adminaddr 1>/dev/null 2>&1; then
     echo "root: $(/native/usr/sbin/mdata-get mail_adminaddr)" >> /etc/aliases
@@ -12,20 +15,24 @@ if /native/usr/sbin/mdata-get mail_smarthost 1>/dev/null 2>&1; then
   echo "$(/native/usr/sbin/mdata-get mail_smarthost):$AUTH" > /etc/exim4/passwd.client
   chmod 0640 /etc/exim4/passwd.client
   
-  sed -i "s:dc_eximconfig_configtype='local':dc_eximconfig_configtype='satellite':" \
+  sed -i \
+    -e "s:dc_eximconfig_configtype='local':dc_eximconfig_configtype='satellite':" \
+    -e "s:dc_smarthost='':dc_smarthost='${HOST}':" \
     /etc/exim4/update-exim4.conf.conf
 
-  sed -i "s:dc_smarthost='':dc_smarthost='$(/native/usr/sbin/mdata-get mail_smarthost)':" \
-    /etc/exim4/update-exim4.conf.conf
+  # -e "s|dc_local_interfaces='127.0.0.1 ; ::1'|dc_local_interfaces='${IP}'|" \
+
 fi
 
-sed -i "s:dc_other_hostnames='debian-8-01':dc_other_hostnames='$(/usr/bin/echo -n `/bin/hostname -f`)':" \
+sed -i "s:dc_other_hostnames='.*':dc_other_hostnames='${HOST}':" \
   /etc/exim4/update-exim4.conf.conf
 
 # cleanup /etc/hosts
 sed -i '/ip6-allnodes/d' /etc/hosts
 sed -i '/ip6-allrouter/d' /etc/hosts
 sed -i '/mibe-lx-basic.qutic.net/d' /etc/hosts
+
+/usr/sbin/update-exim4.conf
 
 hostname > /etc/mailname
 systemctl restart exim4
